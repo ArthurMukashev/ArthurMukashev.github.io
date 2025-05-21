e35c1bad0b32642142abba19780deefefb9f49d8
ArthurMukashev <arthur.mukasev@gmail.com>
2025-05-22 00:18:34 +0500

    заготовка для опроса

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,33 +19,35 @@ let x = setInterval(function () {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  const divStyle = 'text-light bg-dark rounded px-4 py-3 mx-4';
+
   // Display the countdown
   document.getElementById("countdown").innerHTML = `
     ${weeks > 0 ? `
-      <div class="text-light bg-dark rounded px-4 py-3">
+      <div class="${divStyle}">
         <b class="animated">${weeks}</b><br>
         ${weeks > 1 ? 'Недель' : 'Неделя'}
       </div>
     ` : ''}
     ${days > 0 || weeks > 0 ? `
-      <div class="text-light bg-dark rounded px-4 py-3">
+      <div class="${divStyle}">
         <b class="animated">${days}</b><br>
         ${days > 1 ? 'Дней' : 'День'}
       </div>
     ` : ''}
     ${hours > 0 || days > 0 || weeks > 0 ? `
-      <div class="text-light bg-dark rounded px-4 py-3">
+      <div class="${divStyle}">
         <b class="animated">${hours}</b><br>
         ${hours > 1 ? 'Часов' : 'Час'}
       </div>
     ` : ''}
     ${minutes > 0 || hours > 0 || days > 0 || weeks > 0 ? `
-      <div class="text-light bg-dark rounded px-4 py-3">
+      <div class="${divStyle}">
         <b class="animated">${minutes}</b><br>
         ${minutes > 1 ? 'Минут' : 'Минута'}
       </div>
     ` : ''}
-    <div class="text-light bg-dark rounded px-4 py-3">
+    <div class="${divStyle}">
       <b class="animated">${seconds}</b><br>
       ${seconds > 1 ? 'Секунд' : 'Секунда'}
     </div>
@@ -54,6 +56,5 @@ let x = setInterval(function () {
   // If the countdown is finished
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
